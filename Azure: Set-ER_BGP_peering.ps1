b6cc8f9c0a49775bfcfb9913c556104a5c1148ba
Atanu Mandal <46714708+atanulearning@users.noreply.github.com>
2024-02-14 22:02:12 +0800

    Create Azure: Set-ER_BGP_peering.ps1

---
+++ Azure: Set-ER_BGP_peering.ps1
@@ -0,0 +1,8 @@
+Get-AzExpressRouteCircuit -Name "ExpressRouteARMCircuit" -ResourceGroupName "ExpressRouteResourceGroup"
+Add-AzExpressRouteCircuitPeeringConfig -Name "AzurePrivatePeering" -ExpressRouteCircuit $ckt -PeeringType AzurePrivatePeering -PeerASN 100 -PrimaryPeerAddressPrefix "10.0.0.0/30" -SecondaryPeerAddressPrefix "10.0.0.4/30" -VlanId 200
+
+Set-AzExpressRouteCircuit -ExpressRouteCircuit $ckt
+
+
+MD5
+Add-AzExpressRouteCircuitPeeringConfig -Name "AzurePrivatePeering" -ExpressRouteCircuit $ckt -PeeringType AzurePrivatePeering -PeerASN 100 -PrimaryPeerAddressPrefix "10.0.0.0/30" -SecondaryPeerAddressPrefix "10.0.0.4/30" -VlanId 200  -SharedKey "A1B2C3D4"
